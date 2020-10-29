5b0313539f556e188f08318fd66d707a53715d7e
Denner Vidal <denner.vidal@gmail.com>
2020-10-28 20:26:52 -0400

    script de seed

--- src/database/seeds/create_alunos.ts
+++ src/database/seeds/create_alunos.ts
@@ -3,14 +3,14 @@ import Knex from "knex";
 export async function seed(knex: Knex) {
   await knex("aluno").insert([
     {
-      nome: "João",
+      nome: "João Silva",
       rga: "2020.1907.011-2",
       curso: "Sistemas de Informação",
     },
     {
-      nome: "Maria",
-      rga: "2020.1907.011-3",
-      curso: "Engenharia de Software",
+      nome: "Maria Costa",
+      rga: "2020.1905.011-3",
+      curso: "Engenharia de Computação",
     },
   ]);
 }
